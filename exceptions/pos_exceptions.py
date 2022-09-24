590b3af3e195d244eee237c587c112e578f2e340
rohanpandey6 <75936788+rohanpandey6@users.noreply.github.com>
2022-09-25 00:13:10 +0530

    Update pos_exceptions.py

--- exceptions/pos_exceptions.py
+++ exceptions/pos_exceptions.py
@@ -1,2 +1,6 @@
 class ProductNotFound(Exception):
     """Raise when Product ID is not found in catalogue"""
+    
+    
+class ProductAlreadyExists(Exception):
+    """Raise when Product ID is already present in catalogue"""    
