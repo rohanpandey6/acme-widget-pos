fc1c6edf1fb528e4c15312729a133bb1f31829f7
rohanpandey6 <75936788+rohanpandey6@users.noreply.github.com>
2022-09-24 18:53:00 +0530

    Update IOffer.py

--- src/offer_service/IOffer.py
+++ src/offer_service/IOffer.py
@@ -4,4 +4,4 @@ from abc import ABC, abstractmethod
 class OfferStrategy(ABC):
     @abstractmethod
     def apply_offer(self, basket_items):
-        ""
+        """Abstract method for applying offers on Basket Items"""
